cded1089a6266e4f249f06c20ebf998ddd39eb80
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-02-14 01:15:28 +0700

    python file to download testsets from steinlib, sort statistics data

--- src/do_statistics.py
+++ src/do_statistics.py
@@ -57,6 +57,17 @@ def statistics_for_program(program_name):
             result[testname][key] = beautify(result[testname][key])
     return result
 
+testnames_path = "../tests_temp/"
+testnames = []
+testsets = os.listdir(testnames_path)
+for testset in testsets:
+    files = os.listdir(testnames_path + testset)
+    for file in files:
+        if file[-4:] == '.stp':
+            testnames.append(file[:-4])
+
+print(testnames)
+
 focused_programs = ['IGA', 'IGA_F']
 def to_csv(field, statistics_data):
     with open(f"{output_path}{field}.csv", "w", newline='') as csvfile:
@@ -64,7 +75,7 @@ def to_csv(field, statistics_data):
         header_row = ['testname']
         header_row.extend(focused_programs)        
         writer.writerow(header_row)
-        for testname in statistics_data['SGA']:
+        for testname in testnames:
             try:
                 row = [testname]
                 for progname in focused_programs: 
