43bbb3fb7942ac45c2fbd2186f661257b51d0ed7
Le Tang Phu Quy <letangphuquy4@gmail.com>
2024-05-05 01:17:23 +0700

    Bellman-Ford for multi-source SP (make_span)
    Warm start (labels not updated from scratch)

---
+++ src/bellman.hpp
@@ -0,0 +1,53 @@
+#if !defined(BELLMAN_H)
+#define BELLMAN_H
+
+// Source: https://cp-algorithms.com/graph/bellman_ford.html#shortest-path-faster-algorithm-spfa
+
+#include "template.hpp"
+#include "array.hpp"
+#include "graph.hpp"
+// use the shared graph instance
+#include <queue>
+
+class MultipleShortestPath
+{
+private:
+    Array<int> path_edges;
+
+public:
+    MultipleShortestPath() {}
+    // returns distance and corresponding source node
+    vector<pair<int,int>> calc_distance(cst(vector<int>) src, bool first = true) {
+        int n = graph.size();
+        static vector<pair<int,int>> labels;
+        if (first) labels.assign(n+1, std::make_pair(INF, -1));
+        // vector<int> cnt(n, 0);
+        vector<bool> inqueue(n+1, false);
+        std::queue<int> q;
+
+        for (auto s : src) {
+            labels[s] = {0,s};
+            q.push(s); inqueue[s] = true;
+        }
+        while (!q.empty()) {
+            int v = q.front(); 
+            q.pop(); inqueue[v] = false;
+            for (int e : graph[v]) {
+                auto edge = graph.edge(e);
+                int to = edge.other_end(v);
+                int relax = labels[v].first + edge.weight;
+                if (umin(labels[to].first, relax)) {
+                    labels[to].second = labels[v].second; // same source
+                    if (!inqueue[to]) {
+                        q.push(to); inqueue[to] = true;
+                        // cnt[to]++;
+                        // if (cnt[to] > n) return false;  // negative cycle
+                    }
+                }
+            }
+        }
+        return labels;
+    }
+} mssp_handler;
+
+#endif // BELLMAN_H
--- src/testrun.hpp
+++ src/testrun.hpp
@@ -27,7 +27,7 @@ const string TESTSETS[] = {
     "C", 
     "D", 
     "E", 
-    // "I320",
+    "I320",
     "notestset"
 };
 
@@ -50,7 +50,7 @@ const SetType SETS_DEBUG({"MC"});
 const SetType SETS_PROTOTYPE({"SP", "MC", "X", "P4E"});
 const SetType SETS_NEW({"P4E", "P4Z", "P6E", "P6Z", "1R", "2R", "I080", "I160"});
 const SetType SETS_GOOD({"P4E", "P4Z", "P6E", "P6Z"});
-const SetType SETS_BENCHMARK({"SP", "X", "I080", "I160", "C", "D", "E"});
+const SetType SETS_BENCHMARK({"SP", "X", "I080", "I160", "C", "D", "E", "I320"});
 const SetType SETS_MODEST({"C", "D", "E"});
 const SetType SETS_BENCHMARK_ADDITIONAL(SETS_GOOD);
 const SetType SETS_ANTI_REDUCE({"SP", "I080", "I160"});
